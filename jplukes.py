734c220c8fa539ee51023b63525eb15108123b5a
JustinLukes <159123409+JustinLukes@users.noreply.github.com>
2024-11-06 11:04:45 +0100

    final_lukes

---
+++ jplukes.py
@@ -0,0 +1,19 @@
+# 1. Vytvořte pole, které bude mít 5 stringových hodnot.
+pole = ["strom", "řeka", "hora", "jezero", "město"]
+print("Základní list:", pole)
+
+# 2. Přidejte pomocí metody append() jeden prvek. - "vítr"
+pole.append("vítr")
+print("Po Přidání 'vítr':", pole)
+
+# 3. Odstraňte pomocí metody remove() druhý prvek z pole.
+pole.remove(pole[1])  
+print("Po Odstranění druhého stringu:", pole)
+
+# 4. Zaměňte 5. hodnotu z pole za: "slunce"
+pole[4] = "slunce"
+print("Po Zaměnění pátého stringu za 'slunce':", pole)
+
+# 5. Přidejte 2 stringové hodnoty pole pomocí metody extend().
+pole.extend(["měsíc", "hvězda"])
+print("Přidání 'měsíc' a 'hvězda':", pole)
